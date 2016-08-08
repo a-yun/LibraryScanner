5cefcaaeb310cf45f3c13a9fdef09cad0c5a490d
a-yun <antony.n.yun@gmail.com>
2016-08-07 22:19:44 -0500

    Stretch 'Import Database' button

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -112,9 +112,9 @@
             // databaseBrowseButton
             // 
             this.databaseBrowseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.databaseBrowseButton.Location = new System.Drawing.Point(789, 90);
+            this.databaseBrowseButton.Location = new System.Drawing.Point(765, 90);
             this.databaseBrowseButton.Name = "databaseBrowseButton";
-            this.databaseBrowseButton.Size = new System.Drawing.Size(178, 38);
+            this.databaseBrowseButton.Size = new System.Drawing.Size(202, 38);
             this.databaseBrowseButton.TabIndex = 6;
             this.databaseBrowseButton.Text = "Import Database";
             this.databaseBrowseButton.UseVisualStyleBackColor = true;
